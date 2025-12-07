d4a8854cfeb981d1961bde5f6e5a5ddb1fa39ab7
Bardolomeo <sapiobard@gmail.com>
2025-12-07 12:58:46 +0100

    .

--- home.nix
+++ home.nix
@@ -58,103 +58,103 @@
     # networking tools
     dnsutils  # `dig` + `nslookup`
     socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
+			nmap # A utility for network discovery and security auditing
+			ipcalc  # it is a calculator for the IPv4/v6 addresses
 
-    # misc
-    cowsay
-    file
-    which
-    tree
+			# misc
+			cowsay
+			file
+			which
+			tree
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix`
-    # with more details log output
-    nix-output-monitor
+			# nix related
+			#
+			# it provides the command `nom` works just like `nix`
+			# with more details log output
+			nix-output-monitor
 
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
+			# productivity
+			hugo # static site generator
+			glow # markdown previewer in terminal
 
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
+			btop  # replacement of htop/nmon
+			iotop # io monitoring
+			iftop # network monitoring
 
-    # system call monitoring
+			# system call monitoring
 
-    # system tools
+			# system tools
 
-    nix-fast-build
-    nix-eval-jobs
-    
-    # git
+			nix-fast-build
+			nix-eval-jobs
+			
+			# git
 
-    lazygit
-    nix-fast-build
-  ]; 
+			lazygit
+			nix-fast-build
+		]; 
 
-  programs.brave = {
-    enable = true;
-    package = pkgs.brave;
-    extensions = [
-    	# GET THEM FROM CHROME STORE URL
-      "eimadpbcbfnmbkopoojfekhnkhdbieeh"  #dark reader
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm"  # ublock origin
-      "dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
-      "nngceckbapebfimnlniiiahkandclblb" # bitwarden
-      "bmdglhpmdgdibibdlmapblpjbandmpga" # AC Theme
-      "hnlkiofnhhoahaiimdicppgemmmomijo" # qwant
-    ];
-  };
+		programs.brave = {
+			enable = true;
+			package = pkgs.brave;
+			extensions = [
+				# GET THEM FROM CHROME STORE URL
+				"eimadpbcbfnmbkopoojfekhnkhdbieeh"  #dark reader
+				"cjpalhdlnbpafiamejdnhcphjbkeiagm"  # ublock origin
+				"dbepggeogbaibhgnhhndojpepiihcmeb" # vimium
+				"nngceckbapebfimnlniiiahkandclblb" # bitwarden
+				"bmdglhpmdgdibibdlmapblpjbandmpga" # AC Theme
+				"hnlkiofnhhoahaiimdicppgemmmomijo" # qwant
+			];
+		};
 
-  # basic configuration of git, please change to your own
-  programs.git = {
-    enable = true;
-    settings.user.name = "Bardolomeo";
-    settings.user.email = "sapiobard@gmail.com";
-  };
+		# basic configuration of git, please change to your own
+		programs.git = {
+			enable = true;
+			settings.user.name = "Bardolomeo";
+			settings.user.email = "sapiobard@gmail.com";
+		};
 
-  # alacritty - a cross-platform, GPU-accelerated terminal emulator
-  programs.kitty= {
-    enable = true;
-    # custom settings
-  };
+		# alacritty - a cross-platform, GPU-accelerated terminal emulator
+		programs.kitty= {
+			enable = true;
+			# custom settings
+		};
 
-    services.gnome-keyring.enable = true;
-  
-  wayland.windowManager.sway = {
-    enable = true;
-    wrapperFeatures.gtk = true; # Fixes common issues with GTK 3 apps
-    config = rec {
-      modifier = "Mod4";
-      # Use kitty as default terminal
-      terminal = "kitty"; 
-      input."*".xkb_layout = "it";
-      startup = [
-        # Launch Firefox on start
-        {command = "exec sleep 5; systemctl --user start kanshi.service";}
-      ];
-    };
-  };
+			services.gnome-keyring.enable = true;
+		
+		wayland.windowManager.sway = {
+			enable = true;
+			wrapperFeatures.gtk = true; # Fixes common issues with GTK 3 apps
+			config = rec {
+				modifier = "Mod4";
+				# Use kitty as default terminal
+				terminal = "kitty"; 
+				input."*".xkb_layout = "it";
+				startup = [
+					# Launch Firefox on start
+					{command = "exec sleep 5; systemctl --user start kanshi.service";}
+				];
+			};
+		};
 
-  # ouput configuration daemon for displays (Sway)
-  systemd.user.services.kanshi = {
-    environment = {
-      WAYLAND_DISPLAY="wayland-1";
-      DISPLAY = ":0";
-    }; 
-    serviceConfig = {
-      Type = "simple";
-      ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
-    };
-  };
+		# ouput configuration daemon for displays (Sway)
+		systemd.user.services.kanshi = {
+			environment = {
+				WAYLAND_DISPLAY="wayland-1";
+				DISPLAY = ":0";
+			}; 
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = ''${pkgs.kanshi}/bin/kanshi -c kanshi_config_file'';
+			};
+		};
 
-  programs.bash = {
+		programs.bash = {
     enable = true;
     enableCompletion = true;
 		shellAliases = {
-		nix-git = "git add && git commit -m "." && git push && sudo nixos-rebuild --flake 'github:Bardolomeo/nixos#nightqu33n'"
+			nix-git = "git add && git commit -m "." && git push && sudo nixos-rebuild --flake 'github:Bardolomeo/nixos#nightqu33n'";
 		};
     # TODO add your custom bashrc here
 
