04eea0f02310433b267bc42286b9f70b965db78c
Bardolomeo <sapiobard@gmail.com>
2025-12-07 15:56:15 +0100

    .

--- home.nix
+++ home.nix
@@ -37,6 +37,7 @@ in
   xdg.configFile."nvim/".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/nvim;
 	xdg.configFile."sway/config".source = lib.mkForce swayPath;
   xdg.configFile."rofi/".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/rofi;
+  xdg.configFile."waybar/".source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/waybar;
 
 
 
