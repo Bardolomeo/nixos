b80a230bbb367cb6c8f5fd2bdd4faf8e484ebd87
Bardolomeo <sapiobard@gmail.com>
2025-12-08 18:59:47 +0100

    .

--- lsp.nix
+++ lsp.nix
@@ -1,16 +1,16 @@
 { config, pkgs, lib, home, ...} :
 {
-  pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
-      # select Python packages here
-			python-lsp-server
-    ])
 
-	home.packages = with pkgs; [ 
+ 	home.packages = with pkgs; [ 
 		nil
 		tailwindcss-language-server
 		libclang
 		vscode-css-languageserver
 		qt6.qtdeclarative
 		typescript-language-server 
+		python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+			python-lsp-server
+    ])
 	];
 }
