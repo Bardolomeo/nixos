2d3631468f9ec83c1752fe74aac2602a2ae35872
Bardolomeo <sapiobard@gmail.com>
2025-12-09 02:09:30 +0100

    .

--- home.nix
+++ home.nix
@@ -13,7 +13,6 @@ in
 
 	imports = [./lsp.nix]; 
 
-	programs.nixvim.imports = [ ./nixvim.nix ];
   # link the configuration file in current directory to the specified location in home directory
   # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
 
