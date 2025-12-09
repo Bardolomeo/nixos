53ec120c11effcac5de6cce7479918cc9d1e74a6
Bardolomeo <sapiobard@gmail.com>
2025-12-09 01:43:07 +0100

    .

--- home.nix
+++ home.nix
@@ -173,10 +173,26 @@ in
 
   };
 
+		programs.nixvim = {
+			enable = true;
+			
+			extraPlugins = [
+				(pkgs.vimUtils.buildVimPlugin {  
+        name = "powerofneo.nvim";  
+        src = pkgs.fetchFromGitHub {  
+          owner = "Bardolomeo";  
+          repo = I"powerofneo.nvim";  
+          rev = "f75df1447edee57e74faf730ef47ec7a902a787d";  
+          hash = "1r2kvy83550fc62dzlvhq5i0ywxkz4az5nayabbacrki2cl97jfx";  
+        };  
+      })
+			];
+	
+		};
+
 		programs.neovim = {
 			enable = true;
 			plugins = with pkgs.vimPlugins; [
-				powerofneo.nvim
 				nvim-treesitter
 			];
 			extraLuaConfig = ''	vim.cmd.colorscheme("POWEROFNEO") '';
